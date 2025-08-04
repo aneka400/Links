64c598d1cd83c558595c42813fbede87319a5011
aneka400 <kaldybayevanel@gmail.com>
2025-08-04 18:44:28 +0500

    Create eighthproject.py

---
+++ eighthproject.py
@@ -0,0 +1,14 @@
+# https://github.com/aneka400/Links.git
+pets = {
+    "Каа": {
+        "Вид питомца": "желторотый питон",
+        " Возраст питомца": "19 лет",
+        "Имя владельца": "Саша"
+    }
+}
+print(pets.keys(), pets.values())
+
+my_dict = {}
+for i in range(10, -6, -1):
+    my_dict[i] = i ** i
+print(my_dict)
