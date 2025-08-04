c66c8bdae5e6c9ef0556ea3bcb9e9a60d7e480d1
aneka400 <kaldybaevanel@gmail.com>
2025-08-04 07:59:51 +0500

    i try to print

--- synergy.py
+++ synergy.py
@@ -71,3 +71,21 @@ elif  ((Mike + ivan) >= sum):
 else:
     ((Mike + ivan) <= sum)
     print(0)
+
+ N = int(input())
+for i in range(N):
+    print(i + 1, end=" ")
+
+a = 0 
+X = int(input())
+for i in range(X):
+    print(i + 1, end=" ")
+    if i % 2 == 0:
+         a += 1
+print(a) 
+
+A = int(input())
+B = int(input())
+for i in range(A, B + 1):
+    if i % 2 == 0:
+        print(i, end=" ")
